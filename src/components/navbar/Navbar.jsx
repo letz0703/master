28d04669f2f9cdadb3b21b3009dbbcf688359b69
ManI <letz0703@gmail.com>
2022-11-15 16:47:10 +0900

    animista to App.css
    Figma assets for gpt

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
+import {RiMenu3Line, RiCloseLine} from 'react-icons/ri';
 import './navbar.css';
 
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div className="gpt3__navbar">
+      <div className="gpt3__navbar-links">
+        <div className="gpt3__navbar-links_logo">
+          <img src="https://dummyimage.com/100x100/000/fff" alt="logo" />
+        </div>
+      </div>
+    </div>
+  );
 }
